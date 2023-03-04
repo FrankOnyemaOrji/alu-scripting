7a1e37a3f2f88589ca7db6896eb2bfde407d0701
FrankOnyemaOrji <f.orji@alustudent.com>
2023-03-04 19:00:09 +0200

    Corrections done again

--- api_advanced/2-recurse.py
+++ api_advanced/2-recurse.py
@@ -5,26 +5,31 @@ import requests
 
 def recurse(subreddit, after='', hot_list=[], page_count=0):
     """Function that queries the Reddit API."""
-    url = "https://www.reddit.com/r/{}/hot.json?limit=10" \
+    url = "https://www.reddit.com/r/{}/hot.json" \
         .format(subreddit)
     headers = {'User-Agent': 'My User Agent 1.0'}
     response = requests.get(url, headers=headers, parmas=params)
     params = {'limit': 100, 'after': after}
 
     if response.status_code == 200:
-        data = response.json().get('data').get('children')
-        for post in data:
-            hot_list.append(post.get('data').get('title'))
-        after = data.get('after')
+        json_data = response.json()
+
+        for child in json_data.get('data').get('children'):
+            title = child.get('data').get('title')
+            hot_list.append(title)
+
+        after = json_data.get('data').get('after')
         if after is not None:
-            page_count += 1
-            recurse(subreddit, after, hot_list, page_count)
+
+            page_counter += 1
+            return recurse(subreddit, after=after,
+                           hot_list=hot_list, page_counter=page_counter)
         else:
             return hot_list
+
     else:
         return None
 
-if __name__ == "__main__":
-    print(recurse("programming is logical"))
 
-    
+if __name__ == '__main__':
+    print(recurse("zerowastecz"))
--- api_advanced/3-count.py
+++ api_advanced/3-count.py
@@ -8,23 +8,22 @@ def count_words(subreddit, word_list, after='', hot_list=[]):
     """Function that queries the Reddit API."""
     if after == '':
         hot_list = [] * len(word_list)
-    url = "https://www.reddit.com/r/{}/hot.json?limit=100" \
+    url = "https://www.reddit.com/r/{}/hot.json" \
         .format(subreddit)
     request = requests.get(url, params={'after': after},
                            allow_redirects=False,
                            headers={'User-Agent': 'My User Agent 1.0'})
-
     if request.status_code == 200:
         data = request.json()
 
-        for post in data('data').get('children'):
-            for word in post['data']['title'].split():
+        for topic in (data['data']['children']):
+            for word in topic['data']['title'].split():
                 for i in range(len(word_list)):
                     if word_list[i].lower() == word.lower():
                         hot_list[i] += 1
 
         after = data['data']['after']
-        if after is not None:
+        if after is None:
             save = []
             for i in range(len(word_list)):
                 for j in range(i + 1, len(word_list)):
@@ -35,16 +34,17 @@ def count_words(subreddit, word_list, after='', hot_list=[]):
             for i in range(len(word_list)):
                 for j in range(i, len(word_list)):
                     if (hot_list[j] > hot_list[i] or
-                        (word_list[j] > word_list[i] and
-                         hot_list[j] == hot_list[i])):
+                            (word_list[i] > word_list[j] and
+                             hot_list[j] == hot_list[i])):
                         a = hot_list[i]
                         hot_list[i] = hot_list[j]
                         hot_list[j] = a
                         a = word_list[i]
                         word_list[i] = word_list[j]
                         word_list[j] = a
+
             for i in range(len(word_list)):
                 if (hot_list[i] > 0) and i not in save:
-                    print("{}: {}".format(word_list[i], hot_list[i]))
-            else:
-                count_words(subreddit, word_list, after, hot_list)
+                    print("{}: {}".format(word_list[i].lower(), hot_list[i]))
+        else:
+            count_words(subreddit, word_list, after, hot_list)
