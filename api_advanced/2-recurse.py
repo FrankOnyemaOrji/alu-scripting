2fcf2d1a7978bd90dd9fe95519f9f7d4e84b5119
FrankOnyemaOrji <f.orji@alustudent.com>
2023-03-04 18:51:52 +0200

    Corrections done

--- api_advanced/2-recurse.py
+++ api_advanced/2-recurse.py
@@ -8,17 +8,23 @@ def recurse(subreddit, after='', hot_list=[], page_count=0):
     url = "https://www.reddit.com/r/{}/hot.json?limit=10" \
         .format(subreddit)
     headers = {'User-Agent': 'My User Agent 1.0'}
-    response = requests.get(url, headers=headers)
+    response = requests.get(url, headers=headers, parmas=params)
     params = {'limit': 100, 'after': after}
 
     if response.status_code == 200:
         data = response.json().get('data').get('children')
         for post in data:
             hot_list.append(post.get('data').get('title'))
-        after = response.json().get('data').get('after')
+        after = data.get('after')
         if after is not None:
-            return recurse(subreddit, after,
-                           hot_list, page_count + 1)
-        return hot_list
+            page_count += 1
+            recurse(subreddit, after, hot_list, page_count)
+        else:
+            return hot_list
     else:
         return None
+
+if __name__ == "__main__":
+    print(recurse("programming is logical"))
+
+    
--- api_advanced/3-count.py
+++ api_advanced/3-count.py
@@ -12,23 +12,39 @@ def count_words(subreddit, word_list, after='', hot_list=[]):
         .format(subreddit)
     request = requests.get(url, params={'after': after},
                            allow_redirects=False,
-                           headers={'User-Agent': 'My User'})
+                           headers={'User-Agent': 'My User Agent 1.0'})
 
     if request.status_code == 200:
-        data = request.json().get('data').get('children')
-        for post in data:
-            hot_list.append(post.get('data').get('title'))
-        after = request.json().get('data').get('after')
+        data = request.json()
+
+        for post in data('data').get('children'):
+            for word in post['data']['title'].split():
+                for i in range(len(word_list)):
+                    if word_list[i].lower() == word.lower():
+                        hot_list[i] += 1
+
+        after = data['data']['after']
         if after is not None:
-            return count_words(subreddit, word_list, after, hot_list)
-        else:
+            save = []
             for i in range(len(word_list)):
-                word_list[i] = word_list[i].lower()
-            for i in range(len(hot_list)):
-                hot_list[i] = hot_list[i].lower()
+                for j in range(i + 1, len(word_list)):
+                    if word_list[i].lower() == word_list[j].lower():
+                        save.append(j)
+                        hot_list[i] += hot_list[j]
+
             for i in range(len(word_list)):
-                hot_list[i] = hot_list[i].split()
+                for j in range(i, len(word_list)):
+                    if (hot_list[j] > hot_list[i] or
+                        (word_list[j] > word_list[i] and
+                         hot_list[j] == hot_list[i])):
+                        a = hot_list[i]
+                        hot_list[i] = hot_list[j]
+                        hot_list[j] = a
+                        a = word_list[i]
+                        word_list[i] = word_list[j]
+                        word_list[j] = a
             for i in range(len(word_list)):
-                hot_list[i] = hot_list[i].count(word_list[i])
-            for i in range(len(word_list)):
-                print(word_list[i], hot_list[i])
+                if (hot_list[i] > 0) and i not in save:
+                    print("{}: {}".format(word_list[i], hot_list[i]))
+            else:
+                count_words(subreddit, word_list, after, hot_list)
